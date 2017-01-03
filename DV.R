e8a9a32061b676fbe4777a25810127c15f3c48b6
Clement Liu <lsz-clement@hotmail.com>
2017-01-03 02:15:38 -0800

    message

--- DV.R
+++ DV.R
@@ -1,26 +1,40 @@
+#### Notes ####
+#### system reads Date_Col as character, need to convert to date first, Otherwise plotting is incorrect ####
+#windows()
+#xyplot(tail(target_set$RESULT_SET,1000) ~ tail(target_set$DATE_COL,1000), data = target_set, pch = 1, xlab = "Date", ylab = "Count")
+#sort(as.Date(target_set$DATE_COL,format = "%m/%d/%Y"))
+
+#### code ####
 library(lattice)
 library(datasets)
 # Reading the data
-data_set = read.csv("dv test.csv", header = TRUE)
+raw_data = read.csv("dv test.csv", header = TRUE)
 
 # isoldate the value with the desire table name
 target_table = "NOAA_WATER_SUPPLY_FORECAST"
-x = data_set$TABLE_NAME == target_table
-target_set = data_set[x,]
+x = raw_data$TABLE_NAME == target_table
+target_set = raw_data[x,]
 
-tail(target_set,100)
+# setting the rolling back date period
+# set the most recent number of days, 0 means for all days
+days_back = 160
+good_date = as.Date(target_set$DATE_COL,format = "%m/%d/%Y")
 
-daily_count = target_set$RESULT_SET
-date = as.Date(target_set$DATE_COL,format = "%m/%d/%Y")
+if (days_back > 0)
+{
+    rollback_period = good_date >= max(good_date) - days_back
+} else
+{
+    rollback_period = good_date == good_date
+}
 
-date
-summary(as.factor(daily_count))
+rollback_period
+
+daily_count = target_set$RESULT_SET[rollback_period]
+date = good_date[rollback_period]
 
 #ploting graphs
 windows()
 xyplot(daily_count ~ date, data = target_set, pch = 1, xlab = "Date", ylab = "Count")
 windows()
 hist(daily_count)
-
-
-
