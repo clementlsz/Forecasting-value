b03ae3e57a3b369413e9fd67e85eca60319921b9
Clement Liu <lsz-clement@hotmail.com>
2017-01-05 18:48:56 -0800

    message

--- DV.R
+++ DV.R
@@ -4,15 +4,21 @@
 #xyplot(tail(target_set$RESULT_SET,1000) ~ tail(target_set$DATE_COL,1000), data = target_set, pch = 1, xlab = "Date", ylab = "Count")
 #sort(as.Date(target_set$DATE_COL,format = "%m/%d/%Y"))
 
+## work directory ##
+#setwd("C:/Users/clement.liu/Desktop/DV Project/forecasting-value")
+
 #### code ####
 
 library(lattice)
 library(datasets)
-#library(moments)
+
+source("consistency_rate.R")
+
 
 #### Reading the data ####
 
 raw_data = read.csv("dv test.csv", header = TRUE)
+
 
 #### isoldate the values with table name and date ####
 
@@ -22,7 +28,7 @@ target_set = raw_data[x,]
 
 # setting the rolling back date period
 # set the most recent number of days, 0 means for all days
-days_back = 150
+days_back = 0
 good_date = as.Date(target_set$DATE_COL,format = "%m/%d/%Y")
 
 if (days_back > 0)
@@ -39,32 +45,50 @@ date = good_date[recent_days]
 
 #### Analysis ####
 
-# kurtosis(daily_count)
-# create index vector, for split
+## We are dividing the table into small intervals, find out the pattern in each interval and consolidate all the results to check the overall pattern ##
+## Our primary goal is to check whether the distribution is a flat line or not ##
+
+# spliting the data set into intervals #
+# create index vector, for splitting
 index = length(daily_count) : 1
 # set it to every 7 data points, program runs once per week
 rollback_period = 7
-interval = ceiling(index/rollback_period)
-split(daily_count,interval)
-split(date,interval)
+interval = factor(ceiling(index/rollback_period))
+# finding the consistency rate for daily row count in each interval
+daily_count_s = split(daily_count,interval)
+daily_count_l = sapply(daily_count_s, consistency_rate)
+# finding the middle date for each interval
+date_s = split(date,interval)
+date_l = as.Date(sapply(date_s, median),origin = "1970-01-01")
 
+#### ploting graphs for daily row count ####
 
-#### ploting graphs ####
-
-## plot divided into intervals ##
-temp_dataset = data.frame(date, daily_count, interval = factor(interval))
+# plotting count vs time #
 windows()
-xyplot(daily_count ~ date | interval, data = temp_dataset, pch = 1, xlab = "Date", ylab = "Count")
-## plotting chart ##
-windows()
-plot(date, daily_count, col = "blue", pch = 20)
-## plotting linear line ##
+plot(date, daily_count, col = "blue", pch = 20, main = "Daily Count")
+# plotting linear line #
 fit = lm(daily_count ~ date)
 abline(fit, col = "red")
-## boxplot ##
+
+# boxplot and histogram #
 windows()
+par(mfrow = c(2,1))
 boxplot.stats(daily_count)$out
-boxplot(daily_count)
-## histogram ##
+boxplot(daily_count, main = "Daily Count")
+hist(daily_count, main="", prob = TRUE, col = "grey", lwd = 2)
+lines(density(daily_count), main = "Daily Count", col = "blue", lwd = 2)
+
+#### ploting graphs for interval analysis ###
+
+# plot divided into intervals #
+#splitted_dataset = data.frame(date, daily_count, interval)
+#windows()
+#xyplot(daily_count ~ date | interval, data = splitted_dataset, pch = 1, xlab = "Date", ylab = "Count")
+
+# histogram and scatter#
 windows()
-hist(daily_count)
+par(mfrow = c(2,1))
+hist(daily_count_l[2,], main = "Interval Row Count", fre = FALSE, col = "grey", lwd = 2, xlab = "Consistency Rate")
+lines(density(daily_count_l[2,]), col = "blue", lwd = 2)
+
+plot(date_l, daily_count_l[2,], col = "blue", pch = 20, ylab = "Consistency Rate", xlab = "date")
